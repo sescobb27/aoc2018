21e48b88e041e7741492163134236f5531d7c799
sescobb27 <s.escob.b.27@gmail.com>
2018-12-05 11:19:35 -0500

    [add] Day5 part 2

--- lib/day5.ex
+++ lib/day5.ex
@@ -3,31 +3,53 @@ defmodule Day5 do
     input
     |> read_input()
     |> String.codepoints()
-    |> Enum.reduce([], fn
-      char, [] ->
-        [char]
-
-      char, [last_seen | rest] = acc ->
-        result = if (char != last_seen) && (char == String.upcase(last_seen) ||
-             char == String.downcase(last_seen)) do
-          rest
-        else
-          [char | acc]
-        end
-        result
-    end)
-    |> length()
+    |> reduce_polymer()
   end
 
   def part2(input) do
-    input
-    |> read_input()
+    chars =
+      input
+      |> read_input()
+      |> String.codepoints()
+
+    unit_types =
+      chars
+      |> Enum.map(&String.downcase/1)
+      |> Enum.uniq()
+
+    Enum.reduce(unit_types, 1_000_000, fn unit_type, min_size ->
+      without_unit =
+        Enum.reject(chars, fn char ->
+          String.downcase(char) == unit_type
+        end)
+
+      min(min_size, reduce_polymer(without_unit))
+    end)
   end
 
   defp read_input(input) do
     input
     |> File.read!()
     |> String.trim()
+  end
+
+  defp reduce_polymer(chars) do
+    Enum.reduce(chars, [], fn
+      char, [] ->
+        [char]
+
+      char, [last_seen | rest] = acc ->
+        result =
+          if char != last_seen &&
+               (char == String.upcase(last_seen) || char == String.downcase(last_seen)) do
+            rest
+          else
+            [char | acc]
+          end
+
+        result
+    end)
+    |> length()
   end
 end
 
